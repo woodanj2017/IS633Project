7be72350aa7f626a5f1df0a2278ca9603e5b21a3
woodanj2017 <woodanj@uab.edu>
2017-03-09 22:26:16 -0600

    Assignment 1
    This is the initial commit of the project for Assignment 1

---
+++ js/Assignment1.js
@@ -0,0 +1,24 @@
+function MenuChoice(selection)
+{
+    document.getElementById("section1").style.visibility = "hidden";
+    document.getElementById("section1").style.position = "absolute";
+    document.getElementById("section2").style.visibility = "hidden";
+    document.getElementById("section2").style.position = "absolute";
+    
+    switch(selection)
+    {
+        case "Show Area 1":
+            document.getElementById("section1").style.visibility = "visible";
+            document.getElementById("section1").style.position = "relative";
+            break;
+        case "Show Area 2":
+            document.getElementById("section2").style.visibility = "visible";
+            document.getElementById("section2").style.position = "relative";
+            break;
+        case "None":
+            //No menu item selected, so no section will be displayed
+            break;
+        default:
+            alert("Please select a different menu option");
+    }
+}
