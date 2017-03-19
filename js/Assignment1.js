60a3c77f396345b6782d06bc926543395c934718
woodanj2017 <woodanj@uab.edu>
2017-03-19 18:26:45 -0500

    Add Display Properties
    Added display properties to Assignment1.js

--- js/Assignment1.js
+++ js/Assignment1.js
@@ -1,18 +1,22 @@
 function MenuChoice(selection)
 {
     document.getElementById("section1").style.visibility = "hidden";
+    document.getElementById("section1").style.display = "none";
     document.getElementById("section1").style.position = "absolute";
     document.getElementById("section2").style.visibility = "hidden";
+    document.getElementById("section2").style.display = "none";
     document.getElementById("section2").style.position = "absolute";
     
     switch(selection)
     {
         case "Show Area 1":
             document.getElementById("section1").style.visibility = "visible";
+            document.getElementById("section1").style.display = "block";
             document.getElementById("section1").style.position = "relative";
             break;
         case "Show Area 2":
             document.getElementById("section2").style.visibility = "visible";
+            document.getElementById("section2").style.display = "block";
             document.getElementById("section2").style.position = "relative";
             break;
         case "None":
