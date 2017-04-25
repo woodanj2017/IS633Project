65f551fe55f716493954c4339e78ec70cf17e56b
woodanj2017 <woodanj@uab.edu>
2017-04-24 19:58:01 -0500

    Commit
    Commit

--- js/Assignment6.js
+++ js/Assignment6.js
@@ -530,29 +530,29 @@ function onFail(message)
 //            alert("Error: " + err);
 //           });
 //}
-
-function SearchContacts()
-{
-    var options = new ContactFindOptions();
-    options.filter = document.getElementById("contactlastname").value;
-    options.multiple = true;
-    options.desiredFields = [navigator.contacts.fieldType.name];
-    var fields = [navigator.contacts.fieldType.name];
-    navigator.contacts.find(fields, onSuccess, onError, options);
-}
-
-function onSuccess(contacts)
-{
-    var contactinfo = "Contact Names: " + "<br>";
-    for(var count=0; count<contacts.length; count++)
-    {
-        contactinfo += contacts[count].name.givenName + " " + contacts[count].name.familyName + "<br>";
-    }
-    //document.getElementById("contactsearchresults").style.visibility = "visible";
-    document.getElementById("contactsearchresults").innerHTML = contactinfo;
-}
-
-function onError(contactError)
-{
-    alert('onError!');
-}
+//
+//function SearchContacts()
+//{
+//    var options = new ContactFindOptions();
+//    options.filter = document.getElementById("contactlastname").value;
+//    options.multiple = true;
+//    options.desiredFields = [navigator.contacts.fieldType.name];
+//    var fields = [navigator.contacts.fieldType.name];
+//    navigator.contacts.find(fields, onSuccess, onError, options);
+//}
+//
+//function onSuccess(contacts)
+//{
+//    var contactinfo = "Contact Names: " + "<br>";
+//    for(var count=0; count<contacts.length; count++)
+//    {
+//        contactinfo += contacts[count].name.givenName + " " + contacts[count].name.familyName + "<br>";
+//    }
+//    //document.getElementById("contactsearchresults").style.visibility = "visible";
+//    document.getElementById("contactsearchresults").innerHTML = contactinfo;
+//}
+//
+//function onError(contactError)
+//{
+//    alert('onError!');
+//}
